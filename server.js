6e2a794daca20559c3dc58a79e98ef590169e9c0
Firuz43 <firuz1199@mail.ru>
2022-04-21 11:34:07 +0400

    about.html

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method)
 
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/html');
 
-    res.write('Hello there');
-    res.end()
+    //send html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end();
+        }else{
+            res.write(data);
+            res.end();
+        }
+    })
 });
 
 server.listen(3000, 'localhost', () => {

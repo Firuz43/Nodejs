e1ef19e6efa981e448569ff0f44e6e01a517d014
Firuz43 <firuz1199@mail.ru>
2022-04-21 11:09:04 +0400

    view

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
-const server = http.createServer(() => {
-    console.log('request made')
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method)
+
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.write('Hello there');
+    res.end()
 });
 
 server.listen(3000, 'localhost', () => {

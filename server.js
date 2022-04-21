e7f809a8093eb91e8da2af04025629a932f410f1
Firuz43 <firuz1199@mail.ru>
2022-04-21 06:25:50 +0400

    server

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer(() => {
+    console.log('request made')
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for request on 3000')
+})

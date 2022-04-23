0332ab7c2e4101b921a6289dcecf1b86f9627a50
Firuz43 <firuz1199@mail.ru>
2022-04-23 11:04:03 +0400

    express

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
-// console.log(global)
+const express = require('express');
 
-// setTimeout( () => {
-//     console.log('In the timeout');
-//     clearInterval(int);
-// }, 3000)
-
-// const int = setInterval(() => {
-//     console.log('interval')
-// }, 1000)
-
-// console.log(__dirname);
-// console.log(__filename);
-
-const { people, ages } = require('./people');
-
-console.log(people, ages)
-
-const para = require('os');
-
-console.log(para.platform(), para.homedir());
+// express app
+const app = express();
 
